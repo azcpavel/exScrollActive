9c968580563befdbfd033c130bf1261b38cd95b8
Ahsan Zahid Chowdhury <azc.pavel@gmail.com>
2016-07-25 22:22:42 +0600

    Update exScrollActive.js

--- js/exScrollActive.js
+++ js/exScrollActive.js
@@ -7,15 +7,6 @@
 *	@param activeClassName String, Name of the class that will mark as active
 */
 
-/**
-*	@author Ahsan Zahid Chowdhury <itszahid.info>
-*	@description To enable auto active menu on scroll 
-*		data-scrollBefore 	= Number, Mark the item as active before position
-*	@since 2015-11-20
-*	@param className String, Name of the class elements
-*	@param activeClassName String, Name of the class that will mark as active
-*/
-
 function exScrollActive(className, activeClassName){
 	jQuery(window).scroll(function(){
 		var scrollPos = jQuery(window).scrollTop();
